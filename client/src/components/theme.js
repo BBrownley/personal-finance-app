e648159588f28c9d5436e4010e8390ee52311977
BBrownley <brendobrownley@gmail.com>
2024-09-21 03:28:35 -0400

    set up application color theme

---
+++ client/src/components/theme.js
@@ -0,0 +1,24 @@
+export const beige500 = "#98908B";
+export const beige100 = "#F8F4F0";
+
+export const gray900 = "#201F24";
+export const gray500 = "#696868";
+export const gray300 = "#B3B3B3";
+export const gray100 = "#F2F2F2";
+
+export const green = "#277C78";
+export const yellow = "#F2CDAC";
+export const cyan = "#82C9D7";
+export const navy = "#626070";
+export const red = "#C94736";
+export const purpleDark = "#826CB0";
+
+export const purpleLight = "#AF81BA";
+export const turquoise = "#597C7C";
+export const brown = "#93674F";
+export const magenta = "#934F6F";
+export const blue = "#3F82B2";
+export const navyGray = "#97A0AC";
+export const armyGreen = "#7F9161";
+export const gold = "#CAB361";
+export const orange = "#BE6C49";
